c604e6ce84758f405350033f4408a684a1d7843c
robertkoch117 <robertkoch117@gmail.com>
2021-03-25 10:38:02 -0300

    created servicoMensagemSessao.php | padronizando comportamentos com funcoes

--- servicos/servicoValidacao.php
+++ servicos/servicoValidacao.php
@@ -7,23 +7,21 @@ function validaNome(string $nome) : bool{
         
     } else if(strlen($nome)<3){
         $_SESSION['mensagem-de-erro'] = 'O nome não pode conter menos de 3 caracteres';
-        header('location:index.php');
         return false;
         
     } else if(strlen($nome)>40){
         $_SESSION['mensagem-de-erro'] = 'O nome não pode conter mais de 40 caracteres';
-        header('location:index.php');
         return false;
     }
     return true;
 }
 
-function validaIdade(string $idade){
-    else if(!is_numeric($idade)){
+function validaIdade(string $idade) : bool{
+    if(!is_numeric($idade)){
         $_SESSION['mensagem-de-erro'] = 'Informe um número para idade';
-        header('location:index.php');
-        return;
+        return false;
     }
+    return true;
 }
 
  
