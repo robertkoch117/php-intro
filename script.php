b5d4c3e2076562dd8faef058a5ab8534a2de9d9e
robertkoch117 <robertkoch117@gmail.com>
2021-03-26 11:15:38 -0300

    Combinando condicoes com funcoes e retornos | created servicoCategoriaCompetidor.php

--- script.php
+++ script.php
@@ -3,43 +3,6 @@
 
 session_start();
 
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
-$categorias[] = 'idoso';
-//print_r($categorias);
-
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-
-if($idade >= 6 && $idade <= 12){
-    for($i=0; $i<count($categorias); $i++){
-        if($categorias[$i]=='infantil'){
-            $_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome. " compete na categoria ".$categorias[$i];
-            header('location:index.php');
-            return;
-        }
-            
-    }
-    
-} else if ($idade >= 13 && $idade <= 18){
-    for($i=0; $i<count($categorias); $i++){
-        if($categorias[$i]=='adolescente'){
-            $_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome. " compete na categoria adolescente";
-            header('location:index.php');
-            return;
-        }
-            
-    }
-}else{
-    for($i=0; $i<count($categorias); $i++){
-        if($categorias[$i]=='adulto'){
-            $_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome. " compete na categoria adulto";
-            header('location:index.php');
-            return;
-        }
-            
-    }
-}
